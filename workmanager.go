47710cd0cb90ae7f2c19f6e8184635d067d1283b
谢振家 <xiezhenjia@xiezhenjiadeMac-mini.local>
2016-07-25 14:59:13 +0800

    add content

--- workmanager.go
+++ workmanager.go
@@ -47,6 +47,13 @@ func (w *WorkManager) unregister(name string) error {
 	return nil
 }
 
+/***************************************************
+Function:	create a new goroutine
+Parameters:	name		[IN]		the worker name
+			gtnum	[IN]		the goroutine number
+			f		[IN]		worker function
+			res		[OUT]	store the worker result
+****************************************************/
 func (w *WorkManager) NewGoroutine(name string, gtnum int, f WorkFunction, res interface{}) error {
 	err := w.register(name, gtnum, f)
 	if err != nil {
@@ -56,6 +63,11 @@ func (w *WorkManager) NewGoroutine(name string, gtnum int, f WorkFunction, res i
 	return nil
 }
 
+/***************************************************
+Function:	add a request param to the specified worker
+Parameters:	name		[IN]		the worker name
+			req		[IN]		the request param
+****************************************************/
 func (w *WorkManager) AddRequest(name string, req interface{}) error {
 	w.mutex.Lock()
 	defer w.mutex.Unlock()
@@ -68,6 +80,10 @@ func (w *WorkManager) AddRequest(name string, req interface{}) error {
 	return nil
 }
 
+/***************************************************
+Function:	close the specified worker and unregister it
+Parameters:	name		[IN]		the worker name
+****************************************************/
 func (w *WorkManager) Done(name string) error {
 	w.mutex.Lock()
 	if _, ok := w.goworks[name]; !ok {
